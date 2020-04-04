da4b85a58db2f778b4586a286325d8c20ff31bfb
tagine <rana@ranafayez.com>
2020-04-04 19:47:59 -0400

    deployment bug fix

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require("dotenv");
 
-const PORT = 3000;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,9 +16,9 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
